d1c350bbd5c4b4fef8da3ab8db96bee761e0b3cb
Charles Sieg <charles.sieg@renkara.com>
2021-12-03 06:31:34 -0600

    Missed a variable.

--- variables.tf
+++ variables.tf
@@ -17,13 +17,13 @@ variable "alarm_memory_threshold_percentage" {
 }
 
 variable "allowed_cidrs" {
-  default = []
+  default     = []
   description = "A list of CIDRs to allow access to."
   type        = list(string)
 }
 
 variable "additional_security_groups" {
-  default = []
+  default     = []
   description = "A list of Security Group IDs to allow access to."
   type        = string
 }
@@ -60,7 +60,7 @@ variable "automatic_failover_enabled" {
 
 
 variable "availability_zones" {
-  default = []
+  default     = []
   description = "A list of EC2 availability zones in which the replication group's cache clusters will be created. The order of the availability zones in the list is not important."
   type        = list(string)
 }
@@ -154,8 +154,11 @@ variable "replication_group_id" {
   type        = string
 }
 
-snapshot_arns
-
+variable "snapshot_arns" {
+  default     = []
+  description = "A list of Amazon Resource Names (ARNs) that identify Redis RDB snapshot files stored in Amazon S3. The names object names cannot contain any commas."
+  type        = list(string)
+}
 
 variable "snapshot_name" {
   default     = ""
@@ -174,7 +177,7 @@ variable "snapshot_window" {
   description = "The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your cache cluster. The minimum snapshot window is a 60 minute period."
   type        = string
 }
-  
+
 variable "subnet_ids" {
   description = "Required. List of VPC Subnet IDs for the cache subnet group."
   type        = list(string)
