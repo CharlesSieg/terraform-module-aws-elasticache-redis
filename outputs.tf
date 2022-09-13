842a0e63a08cae9de7084d9d9067b33e5c193343
Charles Sieg <crsieg@gmail.com>
2022-09-13 00:34:21 -0500

    Fixes.

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "id" {
-  value = aws_elasticache_replication_group.main.id
+  value = aws_elasticache_replication_group.this.id
 }
 
 output "port" {
@@ -7,17 +7,17 @@ output "port" {
 }
 
 output "primary_endpoint" {
-  value = aws_elasticache_replication_group.main.primary_endpoint_address
+  value = aws_elasticache_replication_group.this.primary_endpoint_address
 }
 
 output "reader_endpoint" {
-  value = aws_elasticache_replication_group.main.reader_endpoint_address
+  value = aws_elasticache_replication_group.this.reader_endpoint_address
 }
 
 output "security_group_id" {
-  value = aws_security_group.main.id
+  value = aws_security_group.this.id
 }
 
 output "subnet_group_name" {
-  value = aws_elasticache_subnet_group.main.name
+  value = aws_elasticache_subnet_group.this.name
 }
